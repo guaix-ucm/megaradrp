3c7b10a0dc8d82e9da63a8e76b20f5ec7bc0d465
Sergio Pascual <sergiopr@fis.ucm.es>
2014-04-11 21:59:41 +0200

    Drop setuptools

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from distutils.core import setup
 
-BASE_PKGS=find_packages('src', exclude=['drp', 'drp.*'])
+BASE_PKGS=['megara', 'megara.drp', 'megara.drp.recipes']
 NAMESPACE_PKGS = ['numina.pipelines', 'numina.pipelines.megara']
 ALL_PKGS = BASE_PKGS + NAMESPACE_PKGS
 
 setup(name='megaradrp',
-      version='0.2.0dev',
+      version='0.2dev',
       author='Sergio Pascual',
       author_email='sergiopr@fis.ucm.es',
       url='http://guaix.fis.ucm.es/hg/pymegara',
       license='GPLv3',
-      description='Megara Data Reduction Pipeline',
+      description='MEGARA Data Reduction Pipeline',
       packages=ALL_PKGS,
       package_dir={'megara': 'src/megara', 'numina.pipelines': 'src/drp'},
       package_data={'megara.drp': ['drp.yaml', 'primary.txt']},
-      install_requires=['numina>=0.11.0'],
+#      install_requires=['numina>=0.11.0'],
       classifiers=[
                    "Programming Language :: Python :: 2.7",
                    'Development Status :: 3 - Alpha',
