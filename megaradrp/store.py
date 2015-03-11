778c1d941d39326b579eaaed22f14966229f6e14
Sergio Pascual <sergiopr@fis.ucm.es>
2015-03-11 02:18:04 +0100

    Add load/dump support

--- megaradrp/store.py
+++ megaradrp/store.py
@@ -17,20 +17,38 @@
 # along with Megara DRP.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import logging
+
 import yaml
 
 from numina.user.dump import dump
+from numina.user.load import load
 
 from .products import TraceMap
 
+_logger = logging.getLogger('megaradrp')
+
+
+_logger.debug('register dump functions')
+
 
 @dump.register(TraceMap)
-def _(tag, obj, where):
+def _d(tag, obj, where):
 
     filename = where.destination + '.yaml'
 
     with open(filename, 'w') as fd:
-        yaml.dump(obj.traces, fd)
+        yaml.dump(obj, fd)
 
     return filename
 
+_logger.debug('register load functions')
+
+
+@load.register(TraceMap)
+def _l(tag, obj):
+
+    with open(obj, 'r') as fd:
+        traces = yaml.load(fd)
+
+    return traces
