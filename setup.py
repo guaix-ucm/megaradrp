961b3abfd95d8f6544d3ebcd42109ba6291508df
Sergio Pascual <sergiopr@fis.ucm.es>
2015-05-28 23:16:41 +0200

    Rename txt files to rst where it make sense

--- setup.py
+++ setup.py
@@ -59,5 +59,5 @@ setup(name='megaradrp',
                    "Operating System :: OS Independent",
                    "Topic :: Scientific/Engineering :: Astronomy",
                    ],
-    long_description=open('README.txt').read()
+    long_description=open('README.rst').read()
 )
