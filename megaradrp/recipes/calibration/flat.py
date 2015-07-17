10558365b7232a36c7f8fa7b1e9df65a10520dfb
Sergio Pascual <sergiopr@fis.ucm.es>
2015-07-17 15:48:50 +0200

    Flat field recipe requires a trace map

--- megaradrp/recipes/calibration/flat.py
+++ megaradrp/recipes/calibration/flat.py
@@ -26,7 +26,7 @@ import logging
 import numpy
 from astropy.io import fits
 
-from numina.core import Product
+from numina.core import Product, Requirement
 from numina.core.products import ArrayType
 from numina.core.requirements import ObservationResultRequirement
 from numina.array.combine import median as c_median
@@ -99,10 +99,10 @@ class FiberFlatRecipe(MegaraBaseRecipe):
     # Requirements
     master_bias = MasterBiasRequirement()
     obresult = ObservationResultRequirement()
+    tracemap = Requirement(TraceMap, 'Trace information of the Apertures')
     # Products
     fiberflat_frame = Product(MasterFiberFlat)
     fiberflat_rss = Product(MasterFiberFlat)
-    traces = Product(TraceMap)
 
     def __init__(self):
         super(FiberFlatRecipe, self).__init__(
@@ -110,82 +110,25 @@ class FiberFlatRecipe(MegaraBaseRecipe):
         )
 
     def run(self, rinput):
-        return self.process_base1(rinput.obresult, rinput.master_bias)
-
-    
-    def process_base1(self, obresult, master_bias):
         _logger.info('starting fiber flat reduction')
 
-        reduced = process_common(self, obresult, master_bias)
+        reduced = process_common(self, rinput.obresult, rinput.master_bias)
         
-        cstart = 2000
-        step = 2
-    
-        tracemap = self.trace(reduced[0].data, cstart, step)
-        
-        rss = apextract_tracemap(reduced[0].data, tracemap)
-        
-        rss[rss <= 0] = 1
-        
-        rss_norm = rss / rss.mean()
+        _logger.info('extract fibers') 
+        rssdata = apextract_tracemap(reduced[0].data, rinput.tracemap)
+        # FIXME: we are ignoring here all the possible bad pixels
+        # and WL distortion when doing the normalization
+        rssdata /= rssdata.mean()
+        rsshdu = fits.PrimaryHDU(rssdata, header=reduced[0].header)
+        rss = fits.HDUList([rsshdu])
+        _logger.info('extraction completed')
         
         _logger.info('fiber flat reduction ended')
 
         result = self.create_result(fiberflat_frame=reduced,
-                                    fiberflat_rss=fits.PrimaryHDU(rss_norm),
-                                    traces=tracemap)
+                                    fiberflat_rss=rss)
 
         return result
-
-    def trace(self, data, cstart, step):
-
-
-        # fit_traces = domefun(data, cstart=2000, hs=20)
-
-        cstart = cstart
-        hs = 1
-        step1 = step
-        background1 = 10.0
-        npred = 3
-        maxdis1 = 2.0
-
-        _logger.info('find peaks in column %i', cstart)
-
-        central_peaks = init_traces(data, center=cstart, hs=hs,
-                                background=background1, npred=npred)
-
-        
-        _logger.info(' %i peaks found', len(central_peaks))
-
-        tracelist = []
-        if data.dtype.byteorder != '=':
-            _logger.debug('byteswapping image')
-            image2 = data.byteswap().newbyteorder()
-        else:
-            image2 = data
-            
-        _logger.info('trace peaks')
-        for trace in central_peaks.values():
-            x, y, p = trace.start
-            mm = trace(image2, x=x, y=y,
-                         step=step1, hs=hs,
-                         background=background1, maxdis=maxdis1
-                         )
-
-            pfit = numpy.polyfit(mm[:,0], mm[:,1], deg=5)
-            
-            tracelist.append({'fibid': trace.fibid, 'boxid': trace.boxid,
-                              'start':0, 'stop':4095,
-                              'fitparms': pfit.tolist()})
-
-            #plt.title('cython version, fiber %i' % trace.fibid)
-            #plt.plot(mm[:,0], mm[:,1], 'r*')
-            #xpix = np.arange(0, 2000, 1)
-            #p = numpy.poly1d(pfit)
-            #plt.plot(xpix, p(xpix), 'b')
-            #plt.show()    
-
-        return tracelist
 
 
 class TwilightFiberFlatRecipe(MegaraBaseRecipe):
