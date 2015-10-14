a0def4d7ef23d98abe929ef01a9e77acd6443ede
Pica <papicazo@ucm.es>
2015-10-14 10:02:01 +0200

    inspect.getmembers is linked to python's version

--- megaradrp/recipes/calibration/tests/test_cBase.py
+++ megaradrp/recipes/calibration/tests/test_cBase.py
@@ -4,8 +4,11 @@ from megaradrp.recipes.calibration.cBase import MegaraBaseRecipe
 
 
 def test_number_of_methods():
-    # lista = ['RecipeInput', 'RecipeResult', '__call__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'buildRI', 'build_recipe_input', 'configure', 'create_input', 'create_result', 'hdu_creation', 'products', 'requirements', 'run', 'set_base_headers']
-    lista = ['RecipeInput', 'RecipeResult', '__call__', '__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'buildRI', 'build_recipe_input', 'configure', 'create_input', 'create_result', 'hdu_creation', 'products', 'requirements', 'run', 'set_base_headers']
+
+    if sys.version_info>(3,0):
+        lista = ['RecipeInput', 'RecipeResult', '__call__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'buildRI', 'build_recipe_input', 'configure', 'create_input', 'create_result', 'hdu_creation', 'products', 'requirements', 'run', 'set_base_headers']
+    else:
+        lista = ['RecipeInput', 'RecipeResult', '__call__', '__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'buildRI', 'build_recipe_input', 'configure', 'create_input', 'create_result', 'hdu_creation', 'products', 'requirements', 'run', 'set_base_headers']
     methods = inspect.getmembers(MegaraBaseRecipe)
 
     assert len(methods) is len(lista)
