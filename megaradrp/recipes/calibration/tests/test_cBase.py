94a404f4f8bde1aeed411a07e07da81d22e91241
Pica <papicazo@ucm.es>
2015-10-20 13:21:05 +0200

    taggers.py tests have been added

--- megaradrp/recipes/calibration/tests/test_cBase.py
+++ megaradrp/recipes/calibration/tests/test_cBase.py
@@ -11,6 +11,11 @@ def test_number_of_methods():
         lista = ['RecipeInput', 'RecipeResult', '_MegaraBaseRecipe__generate_flow', '__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'bias_process_common', 'buildRI', 'build_recipe_input', 'configure', 'create_input', 'create_result', 'hdu_creation', 'products', 'requirements', 'run', 'set_base_headers']
     methods = inspect.getmembers(MegaraBaseRecipe)
 
+    aux=[]
+    for key, value in methods:
+        aux.append(key)
+    print (aux)
+
     assert len(methods) is len(lista)
 
     lista.sort()
