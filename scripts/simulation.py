c7836dda7436e55456a03b044d4c5d255d68e934
Sergio Pascual <sergiopr@fis.ucm.es>
2016-04-05 14:11:24 +0200

    Move ControlSystem to numinadb

--- scripts/simulation.py
+++ scripts/simulation.py
@@ -22,10 +22,9 @@ from megaradrp.simulation.focalplane import FocalPlane
 from megaradrp.simulation.detector import ReadParams, MegaraDetectorSat
 from megaradrp.simulation.vph import MegaraVPH
 
-from numinadb.dal import Session
-from numinadb.model import MyOb, Frame, Base, ObFact
 
-from sqlalchemy import create_engine
+
+
 
 _logger = logging.getLogger("simulation")
 
@@ -191,102 +190,7 @@ def create_telescope():
     return tel
 
 
-class ControlSystem(object):
-    """Top level"""
-    def __init__(self):
-        self._elements = {}
-        from megaradrp.simulation.factory import PersistentRunCounter
-        self.imagecount = PersistentRunCounter('r00%04d.fits')
-        self.mode = 'null'
-        self.ins = 'MEGARA'
-        self.seqs = megara_sequences()
-        self.dbname = 'processing.db'
-        uri = 'sqlite:///%s' % self.dbname
-        engine = create_engine(uri, echo=False)
-        self.conn = None
-        self.datadir = 'data'
-        Session.configure(bind=engine)
 
-    def register(self, name, element):
-        self._elements[name] = element
-
-    def get(self, name):
-        return self._elements[name]
-
-    def set_mode(self, mode):
-        self.mode = mode
-
-    def run(self, exposure, repeat=1):
-        # self.initdb()
-        if repeat < 1:
-            return
-
-        _logger.info('mode is %s', self.mode)
-        try:
-            thiss = self.seqs[self.mode]
-        except KeyError:
-            _logger.error('No sequence for mode %s', self.mode)
-            raise
-
-        session = Session()
-        now = datetime.datetime.now()
-        ob = MyOb(instrument=self.ins, mode=self.mode, start_time=now)
-        session.add(ob)
-
-        iterf = thiss.run(self, exposure, repeat)
-        count = 1
-        for final in iterf:
-            _logger.info('image %d of %d', count, repeat)
-            name = self.imagecount.runstring()
-            fitsfile = factory.create(final, name, self)
-            _logger.info('save image %s', name)
-            fitsfile.writeto(os.path.join(self.datadir, name), clobber=True)
-            # Insert into DB
-            newframe = Frame()
-            newframe.name = name
-            ob.frames.append(newframe)
-            count += 1
-
-
-        # Update completion time of the OB when its finished
-        ob.completion_time = datetime.datetime.now()
-        # Facts
-
-        from numina.core.pipeline import DrpSystem
-
-        drps = DrpSystem()
-
-        this_drp = drps.query_by_name('MEGARA')
-
-        tagger = None
-        for mode in this_drp.modes:
-            if mode.key == self.mode:
-                tagger = mode.tagger
-                break
-
-        if tagger:
-            current = os.getcwd()
-            os.chdir(self.datadir)
-            master_tags = tagger(ob)
-            os.chdir(current)
-            for k in master_tags:
-                fact = ObFact(key=k, value=master_tags[k])
-                ob.facts.append(fact)
-
-        session.commit()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.imagecount.__exit__(exc_type, exc_val, exc_tb)
-
-    def initdb(self):
-
-        uri = 'sqlite:///%s' % self.dbname
-        engine = create_engine(uri, echo=False)
-
-        Base.metadata.create_all(engine)
 
 
 class AtmosphereModel(object):
@@ -314,6 +218,11 @@ if __name__ == '__main__':
     from megaradrp.simulation.factory import MegaraImageFactory
     from megaradrp.simulation.atmosphere import AtmosphereModel
 
+    try:
+        from numinadb.controldb import ControlSystem
+    except ImportError:
+        from megaradrp.simulation.control import ControlSystem
+
     logging.basicConfig(level=logging.DEBUG)
 
     illum = None
@@ -324,7 +233,7 @@ if __name__ == '__main__':
     telescope.connect(atm)
     factory = MegaraImageFactory()
 
-    control = ControlSystem()
+    control = ControlSystem(factory)
     control.register('MEGARA', instrument)
     control.register('GTC', telescope)
     control.register('megcalib', cu)
