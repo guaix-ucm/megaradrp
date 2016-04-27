087475ea0fe6543b746f8cc09553144297c53b77
Sergio Pascual <sergiopr@fis.ucm.es>
2016-04-27 20:33:12 +0200

    Add a reader of the FITS+UVES format

--- megaradrp/simulation/efficiency.py
+++ megaradrp/simulation/efficiency.py
@@ -18,16 +18,38 @@
 #
 
 
-import numpy as np
+import numpy
 import scipy.interpolate as ii
+import astropy.io.fits as fits
+import astropy.wcs
 
 
 class InterpolFile(object):
 
     def __init__(self, fname, fill_value=0.0):
-        rawdata = np.loadtxt(fname)
+        rawdata = numpy.loadtxt(fname)
         self._interp = ii.interp1d(rawdata[:,0] / 1e4, rawdata[:,1],
                                    bounds_error=False, fill_value=fill_value)
+
+    def __call__(self, wl):
+        return self._interp(wl)
+
+
+class InterpolFitsUVES(object):
+    """Interpolate spectrum in UVES format.
+
+    This is the format of the sky spectrum file
+    """
+    def __init__(self, fname, fill_value=0.0):
+        with fits.open(fname) as hdul:
+            rawdata = hdul[0].data
+            wcs = astropy.wcs.WCS(hdul[0].header)
+            # WL is in Angstroms -> to microns
+            all_wl = wcs.all_pix2world(range(hdul[0].data.size), 0)
+
+        self._interp = ii.interp1d(all_wl[0] / 1e4, numpy.abs(rawdata),
+                                   bounds_error=False,
+                                   fill_value=fill_value)
 
     def __call__(self, wl):
         return self._interp(wl)
