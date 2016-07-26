53dd5117cf842fa448a151d5558e815cc03d8a5b
Sergio Pascual <sergiopr@fis.ucm.es>
2016-07-26 12:37:29 +0200

    Remove "TagFits"

--- megaradrp/processing/aperture.py
+++ megaradrp/processing/aperture.py
@@ -19,7 +19,7 @@
 
 import logging
 
-from numina.flow.processing import TagOptionalCorrector, TagFits
+from numina.flow.processing import Corrector
 
 from megaradrp.core.processing import apextract, apextract_tracemap
 
@@ -27,22 +27,19 @@ from megaradrp.core.processing import apextract, apextract_tracemap
 _logger = logging.getLogger('numina.processing')
 
 
-class ApertureExtractor(TagOptionalCorrector):
+class ApertureExtractor(Corrector):
     """A Node that extracts apertures."""
 
-    def __init__(self, trace, datamodel=None, mark=True,
-                 tagger=None, dtype='float32'):
+    def __init__(self, trace, datamodel=None, dtype='float32'):
 
-        if tagger is None:
-            tagger = TagFits('NUM-MAE', 'MEGARA Aperture extractor')
+        #if tagger is None:
+        #    tagger = TagFits('NUM-MAE', 'MEGARA Aperture extractor')
 
         super(ApertureExtractor, self).__init__(datamodel=datamodel,
-                                                tagger=tagger,
-                                                mark=mark,
                                                 dtype=dtype)
         self.trace = trace
 
-    def _run(self, img):
+    def run(self, img):
         imgid = self.get_imgid(img)
         _logger.debug('extracting apertures in image %s', imgid)
         rss = apextract(img[0].data, self.trace)
@@ -51,24 +48,22 @@ class ApertureExtractor(TagOptionalCorrector):
         return img
 
 
-class ApertureExtractor2(TagOptionalCorrector):
+class ApertureExtractor2(Corrector):
     """A Node that extracts apertures."""
 
     def __init__(self, trace, datamodel=None, mark=True,
                  tagger=None, dtype='float32'):
 
-        if tagger is None:
-            tagger = TagFits('NUM-MAE', 'MEGARA Aperture extractor')
+#        if tagger is None:
+#            tagger = TagFits('NUM-MAE', 'MEGARA Aperture extractor')
         
         self.trace = trace
 
         super(ApertureExtractor2, self).__init__(datamodel=datamodel,
-                                                tagger=tagger,
-                                                mark=mark,
                                                 dtype=dtype)
 
 
-    def _run(self, img):
+    def run(self, img):
         imgid = self.get_imgid(img)
         _logger.debug('extracting (apextract_tracemap) in image %s', imgid)
         rss = apextract_tracemap(img[0].data, self.trace)
