ebc1917b856004b5064c95f9f1714b15d2f59954
Sergio Pascual <sergiopr@fis.ucm.es>
2016-11-28 15:51:26 +0100

    Update MOS acquisition recipe (fixes #26)

--- megaradrp/recipes/auxiliary/acquisitionmos.py
+++ megaradrp/recipes/auxiliary/acquisitionmos.py
@@ -17,104 +17,95 @@
 # along with Megara DRP.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-'''Calibration Recipes for Megara'''
-import logging
+"""Calibration Recipes for Megara"""
+
+
+import math
 
 from numina.core import Product
-from numina.core.products import ArrayType
-import astropy.io.fits as fits
+from numina.array.offrot import fit_offset_and_rotation
 import numpy as np
-import re
 
+from megaradrp.types import ProcessedRSS, ProcessedFrame
 from megaradrp.recipes.scientific.base import ImageRecipe
 from megaradrp.recipes.scientific.lcbmap import Grid
 from megaradrp.types import LCBCalibration
-
-_logger = logging.getLogger('numina.recipes.megara')
 
 
 class AcquireMOSRecipe(ImageRecipe):
     """Process MOS images."""
 
-    master_mos_json = Product(LCBCalibration)
-    master_mos = Product(ArrayType)
+    #master_mos_json = Product(LCBCalibration)
+    #master_mos = Product(ArrayType)
+
+    reduced = Product(ProcessedFrame)
+    #final = Product(ProcessedRSS)
+    final = Product(ProcessedRSS)
+    #sky = Product(ProcessedRSS)
 
     def run(self, rinput):
 
-        _logger.info('starting MOS reduction')
+        self.logger.info('starting AC MOS reduction')
 
-        reduced, rssdata = super(AcquireMOSRecipe, self).run(rinput)
+        reduced2d, reduced1d = super(AcquireMOSRecipe, self).base_run(rinput)
+        # rssdata = rss_data[0].data
 
-        fits.writeto('rss.fits', rssdata, clobber=True)
-        reduced.writeto('reduced.fits')
+        self.save_intermediate_img(reduced2d, 'reduced2d.fits')
+        self.save_intermediate_img(reduced1d, 'reduced1d.fits')
 
-        bundles = []
+        do_sky_subtraction = True
+        if do_sky_subtraction:
+            self.logger.info('start sky subtraction')
+            final, origin, sky = self.run_sky_subtraction(reduced1d)
+            self.logger.info('end sky subtraction')
+        else:
+            final =  reduced1d
+            origin = final
+            sky = final
 
-        for key, value in reduced[1].header.items():
-            bun = re.match(r"BUN(\d+)_T", key)
-            if bun:
-                if 'STAR' in value:
-                    bundles.append(bun.group(1))
+        fiberconf = self.datamodel.get_fiberconf(final)
 
-        _logger.info("bundles: %s", bundles)
+        cut1 = 1000
+        cut2 = 3000
+        self.logger.debug("MOS configuration is %s", fiberconf.conf_id)
+        rssdata = final[0].data
+        p1 = []
+        q1 = []
+        for key, bundle in fiberconf.bundles.items():
+            if bundle.target_type == 'STAR':
+                self.logger.debug("%s %s %s", key, bundle.target_name, bundle.target_type)
+                mm = bundle.fibers.values()
+                central_fiber = mm[3] # Central fiber is number 4 in the list
+                central_coords = [central_fiber.x, central_fiber.y]
+                #central_fiber_pair_id
+                # Central fiber is
+                self.logger.debug('Center fiber is %d', central_fiber.fibid)
 
-        headers_list = np.array(reduced[1].header.items())
-        bundles = [i.lstrip('0') for i in bundles]
+                colids = []
+                coords = []
+                for fiber in bundle.fibers.values():
+                    colids.append(fiber.fibid - 1)
+                    coords.append((fiber.x - central_fiber.x,
+                                   fiber.y - central_fiber.y)
+                                  )
+                flux_per_cell = rssdata[colids, cut1:cut2].mean(axis=1)
 
-        final_centroids = []
-        final_sky = []
+                flux_per_cell_total = flux_per_cell.sum()
+                #print(colids)
+                #print(flux_per_cell / flux_per_cell_total)
+                #print(coords)
+                self.logger.debug('Compute centroid: %s', central_fiber.fibid)
+                centroid = np.dot(np.asarray(coords).T, flux_per_cell) / flux_per_cell_total
+                # Compute second order moments
+                # I have this done somewhere
+                print(key, central_coords, central_coords + centroid)
+                p1.append(central_coords)
+                q1.append(central_coords + centroid)
 
-        fibers = []
-        peaks = []
-        second_order = []
-        cova = []
+        offset, rot = fit_offset_and_rotation(np.array(p1), np.array(q1))
+        angle = math.atan2(rot[1, 0], rot[0, 0])
+        self.logger.info('offset is %s', offset)
+        self.logger.info('rot matrix is %s', rot)
+        self.logger.info('rot angle %s', angle)
 
-        for elem in bundles:
-            _logger.info('Bundle: %s', elem)
-            spaxels_x = []
-            spaxels_y = []
-            spaxels_b = []
-            fiber = []
-
-            for aux in headers_list[np.where(headers_list[:, 1] == elem)]:
-                fibb = re.match(r"FIB(\d+)_B", aux[0])
-                if fibb:
-                    _logger.info('FIB%s_X: %s', fibb.group(1), reduced[1].header[
-                        'FIB' + fibb.group(1) + '_X'])
-                    _logger.info('FIB%s_Y: %s', fibb.group(1), reduced[1].header[
-                        'FIB' + fibb.group(1) + '_Y'])
-
-                    spaxels_x.append(
-                        reduced[1].header['FIB' + fibb.group(1) + '_X'])
-                    spaxels_y.append(
-                        reduced[1].header['FIB' + fibb.group(1) + '_Y'])
-                    fiber.append(int(fibb.group(1)))
-                    spaxels_b.append(elem)
-
-            spaxels = np.array(zip(spaxels_x, spaxels_y, spaxels_b, fiber)).astype(float)
-            grid = Grid(spaxels)
-            spaxels_aux = np.array(zip(fiber, spaxels_b, spaxels_x, spaxels_y)).astype(float)
-            vecinos = spaxels[:, 0]
-
-            centroid, sky, peak, sec_ord, cov = self.get_wcallib(100, 1000,
-                                                                 vecinos,
-                                                                 rinput.wlcalib,
-                                                                 rssdata,
-                                                                 spaxels_aux, grid)
-
-            final_centroids.append(centroid)
-            final_sky.append(sky)
-            fibers.append(spaxels[grid.get_fiber(centroid),:][-1])
-            peaks.append(peak)
-            second_order.append(sec_ord)
-            cova.append(cov)
-
-        #FIXME: points should be taken from the coordenates. Calculate which ones
-        points = [[0,0], [1,1], [2,2]]
-        tabla_final = self.generate_solution(points, final_centroids,
-                                             final_sky, fibers, peaks,
-                                             second_order, cova)
-
-        master_mos_json = self.generateJSON(points, final_centroids, final_sky, fibers, peaks, second_order, cova )
-
-        return self.create_result(master_mos=tabla_final, master_mos_json=master_mos_json)
+        return self.create_result(reduced=reduced2d, final=final)
