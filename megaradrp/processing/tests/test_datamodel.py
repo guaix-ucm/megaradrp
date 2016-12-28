f388fade40dfd825f271d6396bd94d2071620c4b
Sergio Pascual <sergiopr@fis.ucm.es>
2016-12-28 20:40:34 +0100

    Fix broken test

--- megaradrp/processing/tests/test_datamodel.py
+++ megaradrp/processing/tests/test_datamodel.py
@@ -11,7 +11,7 @@ def create_empty_img(insmode):
     return img
 
 
-def test_fiberconf():
+def test_fiberconf_LCB():
 
     datamodel = MegaraDataModel()
 
@@ -27,11 +27,27 @@ def test_fiberconf():
     assert conf.nfibers ==  623
 
 
-def test_fiberconf_empty_mos():
+def test_fiberconf_MOS():
 
     datamodel = MegaraDataModel()
 
     img = create_empty_img('MOS')
+
+    conf = datamodel.get_fiberconf(img)
+
+    assert isinstance(conf, FibersConf)
+    # Default values from file
+    assert conf.name == 'MOS'
+    assert conf.conf_id == 1
+    assert conf.nbundles == 92
+    assert conf.nfibers ==  644
+
+
+def test_fiberconf_other():
+
+    datamodel = MegaraDataModel()
+
+    img = create_empty_img('OTHER')
 
     with pytest.raises(ValueError):
         datamodel.get_fiberconf(img)
