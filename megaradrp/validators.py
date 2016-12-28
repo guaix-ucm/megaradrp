770080b5a36fb72a35b47c986fc602aa1a1a34b4
Sergio Pascual <sergiopr@fis.ucm.es>
2016-12-28 17:06:59 +0100

    Check that FOCUS is a number, not string

--- megaradrp/validators.py
+++ megaradrp/validators.py
@@ -32,7 +32,10 @@ def validate_focus(obresult):
     for idx, frame in enumerate(obresult.frames):
         with frame.open() as img:
             try:
-                focus_val = img[0].header['focus']
+                focus_val = img[0].header['FOCUS']
+                # FIXME: This should be done using an scheme for MEGARA
+                if not isinstance(focus_val, (int, float)):
+                    raise ValidationError("FOCUS must be integer, not {}".format(type(focus_val)))
                 if focus_val not in image_groups:
                     image_groups[focus_val] = []
                 image_groups[focus_val].append(frame)
