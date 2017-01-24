5063677f61009e4420f6e3a2fc50a247ea45a018
Sergio Pascual <sergiopr@fis.ucm.es>
2017-01-24 13:29:44 +0100

    Add a focus mechanism to the telescope device in simulation

--- megaradrp/simulation/telescope.py
+++ megaradrp/simulation/telescope.py
@@ -21,6 +21,26 @@ import math
 
 from .device import HWDevice
 from .efficiency import Efficiency
+from .instrument import FocusActuator
+
+
+class M2FocusActuator(FocusActuator):
+    def __init__(self, name, parent=None):
+        super(M2FocusActuator, self).__init__(name, parent)
+
+        # Focus
+        self.internal_focus_factor = 1.0
+        self._ref_focus = 0
+        self._internal_focus = self._ref_focus
+
+    def set_focus(self, x):
+        """Arbitrary parametrization of the focus"""
+        if x < -3000 or x > 3000:
+            raise ValueError('telescope focus out of limits')
+
+        self.internal_focus_factor = 1+ 1.9 * (math.cosh((x - self._ref_focus) / 3000.0) - 1)
+        self._internal_focus = x
+
 
 class Telescope(HWDevice):
 
@@ -36,8 +56,15 @@ class Telescope(HWDevice):
         else:
             self._transmission = transmission
 
+        self.focus_actuator = M2FocusActuator('Focus', self)
+
     def transmission(self, wl):
         return self._transmission.response(wl)
 
     def connect(self, atmmodel):
         self.inc = atmmodel
+
+    def set_focus(self, x):
+        """Arbitrary parametrization of the focus"""
+
+        self.focus_actuator.set_focus(x)
