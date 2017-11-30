d7907705c448922ac752aa3116c2c91a957969d0
Sergio Pascual <sergiopr@fis.ucm.es>
2017-11-30 16:25:55 +0100

    Create a normal function to generate ds9 region for traces

--- megaradrp/products/modelmap.py
+++ megaradrp/products/modelmap.py
@@ -15,6 +15,7 @@ import numpy.polynomial.polynomial as nppol
 from numina.util.convertfunc import json_serial_function, convert_function
 
 from megaradrp.products.structured import BaseStructuredCalibration
+from .traces import to_ds9_reg as to_ds9_reg_function
 
 
 class GeometricModel(object):
@@ -117,51 +118,12 @@ class ModelMap(BaseStructuredCalibration):
             Abscissae where the fibid is shown (default=0 -> not shown).
 
         """
+        return to_ds9_reg_function(self, ds9reg,
+                                   rawimage=rawimage,
+                                   numpix=numpix,
+                                   fibid_at=fibid_at
+                                   )
 
-        # offset between polynomial and image abscissae
-        if rawimage:
-            ix_offset = 51
-        else:
-            ix_offset = 1
-
-        # open output file and insert header
-
-        ds9reg.write('# Region file format: DS9 version 4.1\n')
-        ds9reg.write(
-            'global color=green dashlist=2 4 width=2 font="helvetica 10 '
-            'normal roman" select=1 highlite=1 dash=1 fixed=0 edit=1 '
-            'move=1 delete=1 include=1 source=1\n')
-        ds9reg.write('physical\n')
-        ds9reg.write('#\n# insmode: {0}\n'.format(self.tags['insmode']))
-        ds9reg.write('# vph: {0}\n'.format(self.tags['vph']))
-        ds9reg.write('# uuid: {0}\n'.format(self.uuid))
-        colorbox = ['#ff77ff', '#4444ff']
-        for fiberdict in self.contents:
-            fibid = fiberdict.fibid
-            boxid = fiberdict.boxid
-            xmin = fiberdict.start
-            xmax = fiberdict.stop
-            ds9reg.write('#\n# fibid: {0}\n'.format(fibid))
-            # skip fibers without trace
-            if fiberdict.valid:
-                xp = numpy.linspace(start=xmin, stop=xmax, num=numpix)
-                yp = fiberdict.polynomial(xp)
-                if rawimage:
-                    lcut = (yp > 2056.5)
-                    yp[lcut] += 100
-                for i in range(len(xp) - 1):
-                    x1 = xp[i] + ix_offset
-                    y1 = yp[i] + 1
-                    x2 = xp[i + 1] + ix_offset
-                    y2 = yp[i + 1] + 1
-                    ds9reg.write('line {0} {1} {2} {3}'.format(x1, y1, x2, y2))
-                    ds9reg.write(' # color={0}\n'.format(colorbox[boxid % 2]))
-                    if fibid_at != 0:
-                        if x1 <= fibid_at <= x2:
-                            ds9reg.write('text {0} {1} {{{2}}} # color=green '
-                                         'font="helvetica 10 bold '
-                                         'roman"\n'.format((x1+x2)/2,
-                                                           (y1+y2)/2, fibid))
 
 
 # BUILD MATRICES
