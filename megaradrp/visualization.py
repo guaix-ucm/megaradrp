c1480846133df448fb9f3421608f7a4b14e9509d
Sergio Pascual <sergiopr@fis.ucm.es>
2018-02-27 20:06:25 +0100

    Add command line options to visualization script

--- megaradrp/visualization.py
+++ megaradrp/visualization.py
@@ -16,6 +16,8 @@ import matplotlib.transforms as mtrans
 import matplotlib.transforms as mtransforms
 import numpy as np
 
+from megaradrp.processing.wcs import update_wcs_from_ipa, compute_pa_from_ipa
+
 
 M_SQRT3 = math.sqrt(3)
 
@@ -116,40 +118,53 @@ def hexplot(axis, x, y, z, scale=1.0, extent=None,
     return collection
 
 
-def _demo():
-    import matplotlib.pyplot as plt
-    import megaradrp.datamodel as dm
-    import pkgutil
-    from six import StringIO
-    import astropy.io.fits as fits
+def transform_to_pixel(wcs, coords, ref=1):
+    # We need only the WL axis
+    coords_m = [coords, np.zeros_like(coords)]
+    r = wcs.wcs_world2pix(np.transpose(coords_m), ref)
+    first_col = r[:,0].astype('int')
+    return first_col
 
-    data = pkgutil.get_data('megaradrp', 'lcb_default_header.txt')
 
-    default_hdr = StringIO(data.decode(encoding='utf8'))
-    hdr_fiber = fits.header.Header.fromfile(default_hdr)
-    fiberconf = dm.read_fibers_extension(hdr_fiber)
+def is_inside(shape, *coords):
 
-    PLATESCALE = 1.2120  # arcsec / mm
-    SCALE = 0.443  # mm from center to center, upwards
+    for coord in coords:
+        for shape1d, crds in zip(shape, coord):
+            if not is_inside_1d(shape1d, crds):
+                return False
+    return True
 
-    x = []
-    y = []
-    for fiber in fiberconf.fibers.values():
-        x.append(-fiber.x)
-        y.append(-fiber.y)
 
-    z = np.random.normal(1000, 100, size=len(x))
-    z[0:7] = 0
-    plt.subplots_adjust(hspace=0.5)
-    plt.subplot(111)
-    ax = plt.gca()
-    plt.xlim([-8, 8])
-    plt.ylim([-8, 8])
-    col = hexplot(ax, x, y, z, scale=SCALE, cmap=plt.cm.YlOrRd_r)
-    plt.title("Fiber map")
-    cb = plt.colorbar(col)
-    cb.set_label('counts')
-    plt.show()
+def is_inside_1d(shape1d, coords):
+    for coord in coords:
+        if coord < 0:
+            return False
+        if coord >= shape1d:
+            return False
+    return True
+
+
+def extract_region(wcs_wl, region, rssdata):
+    cut1, cut2 = transform_to_pixel(wcs_wl, region)
+    # print(args.average_region, cut1, cut2)
+
+    if is_inside_1d(rssdata.shape[1], [cut1, cut2]):
+        # cut1, cut2 = args.average_region
+        z = rssdata[:, cut1:cut2].mean(axis=1)
+        return z
+    else:
+        raise ValueError('region to average is outside image')
+
+
+def extract_column(wcs_wl, col, rssdata):
+    cut, = transform_to_pixel(wcs_wl, [col])
+
+    if is_inside_1d(rssdata.shape[1], [cut]):
+        # cut1, cut2 = args.average_region
+        z = rssdata[:, cut]
+        return z
+    else:
+        raise ValueError('column is outside image')
 
 
 def main(argv=None):
@@ -160,10 +175,32 @@ def main(argv=None):
     import matplotlib.pyplot as plt
     import astropy.io.fits as fits
     from astropy.wcs import WCS
+    from astropy.visualization import simple_norm
 
     import megaradrp.datamodel as dm
 
     parser = argparse.ArgumentParser(description='Display MEGARA RSS images')
+    parser.add_argument('--wcs-grid', action='store_true',
+                        help='Display WCS grid')
+    parser.add_argument('--wcs-pa-from-header', action='store_true',
+                        help="Use PA angle from PC keys", dest='pa_from_header')
+    parser.add_argument('--average-region', nargs=2, default=[1000, 3000],
+                        type=int)
+    parser.add_argument('--extname', '-e', default='PRIMARY')
+    parser.add_argument('--column', '-c', type=int)
+    parser.add_argument('--continuum-region', nargs=2,
+                        type=int)
+    parser.add_argument('--coordinate-type', choices=['pixel', 'wcs'],
+                        default='pixel')
+    parser.add_argument('--colormap', type=plt.get_cmap)
+    parser.add_argument('--title')
+    parser.add_argument('--min-cut', type=float)
+    parser.add_argument('--max-cut', type=float)
+    parser.add_argument('--percent', type=float)
+    parser.add_argument('--stretch',
+                        choices=['linear', 'sqrt', 'power', 'log', 'asinh'],
+                        default='linear'
+                        )
     parser.add_argument('rss', metavar='RSS', nargs='+',
                         help='RSS images to process')
 
@@ -171,32 +208,92 @@ def main(argv=None):
 
     for fname in args.rss:
         with fits.open(fname) as img:
+            extname = args.extname
+            # image checks
+            bunit = img[extname].header.get('BUNIT', 'counts')
+            # insmode = img[extname].header.get('INSMODE', 'LCB')
+            # Image shape
+
+            if args.coordinate_type == 'wcs':
+                # read WCS from extname
+                wcs_wl = WCS(img[extname].header)
+            else:
+                wcs_wl = WCS(naxis=len(img[extname].shape))
+
             datamodel = dm.MegaraDataModel()
             fiberconf = datamodel.get_fiberconf(img)
+
             x = []
             y = []
-            for fiber in fiberconf.fibers.values():
+            # Key is fibid
+            for _, fiber in sorted(fiberconf.fibers.items()):
                 x.append(fiber.x)
                 y.append(fiber.y)
-            cut1 = 1000
-            cut2 = 3000
-            z = img[0].data[:, cut1:cut2].mean(axis=1)
 
-            wcs2 = WCS(img['FIBERS'].header)
+            rssdata = np.squeeze(img[extname].data)
+
+            # Handle different dimensionality
+            if rssdata.ndim == 1:
+                # data is 1d
+                print('RSS is 1D')
+                zval = rssdata
+            elif rssdata.ndim == 2:
+                print('coordinates in', args.coordinate_type)
+                if args.column is not None:
+                    print('use column', args.column)
+                    zval = extract_column(wcs_wl, args.column, rssdata)
+                else:
+                    print('compute average in', args.average_region)
+                    zval = extract_region(wcs_wl, args.average_region,
+                                          img[extname].data)
+                # Subtract continuum
+                if args.continuum_region:
+                    print('compute continuum in', args.continuum_region)
+                    z0 = extract_region(wcs_wl, args.continuum_region,
+                                        img[extname].data)
+                    zval -= z0
+            else:
+                raise ValueError('image must be 1D or 2D')
 
             fig = plt.figure()
 
+            projection = None
+
+            if args.wcs_grid:
+                print('display wcs grid')
+                if not args.pa_from_header:
+                    print('compute PA from IPA')
+                    ipa = img[extname].header['IPA']
+                    pa = compute_pa_from_ipa(ipa)
+                    print('IPA is', ipa, ' PA is', pa)
+                    hdr = update_wcs_from_ipa(img['FIBERS'].header, pa)
+                else:
+                    hdr = img['FIBERS'].header
+
+                projection = WCS(hdr)
+
             # plt.subplots_adjust(hspace=0.5)
-            ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=wcs2)
-            ax.set_xlim([-6, 6])
-            ax.set_ylim([-6, 6])
+            ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=projection)
+            ax.set_xlim([-6.5, 6.5])
+            ax.set_ylim([-6.5, 6.5])
 
-            ax.coords.grid(color='black', alpha=1.0, linestyle='solid')
-            col = hexplot(ax, x, y, z, scale=SCALE, cmap=plt.cm.YlOrRd_r)
+            if args.wcs_grid:
+                ax.coords.grid(color='black', alpha=1.0, linestyle='solid')
 
-            plt.title("LCB")
+            norm = simple_norm(zval, args.stretch,
+                               min_cut=args.min_cut,
+                               max_cut=args.max_cut,
+                               percent=args.percent
+                               )
+
+            col = hexplot(ax, x, y, zval, scale=SCALE, cmap=args.colormap, norm=norm)
+
+            if args.title is not None:
+                plt.title(args.title)
+
             cb = plt.colorbar(col)
-            cb.set_label('counts')
+            cb.set_label(bunit)
+
             plt.show()
 
 
