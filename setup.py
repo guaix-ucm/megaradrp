a9f079890fa0e557b5a14703a8a0809501bafe6d
Sergio Pascual <sergiopr@fis.ucm.es>
2018-12-10 17:26:07 +0100

    Update setup.py with python 3.7 [skip ci]

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         'Development Status :: 3 - Alpha',
         "Environment :: Console",
         "Intended Audience :: Science/Research",
