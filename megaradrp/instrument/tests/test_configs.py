8ca72ff8866ff5ff16488a5b23cc6939676f4f54
Sergio Pascual <sergiopr@fis.ucm.es>
2020-06-02 13:35:19 +0200

    Reorder instrument conf tests

--- megaradrp/instrument/tests/test_configs.py
+++ megaradrp/instrument/tests/test_configs.py
@@ -1,6 +1,18 @@
 import pytest
+import astropy.io.fits as fits
 import numina.instrument.generic
+import numina.core
 from megaradrp.loader import load_drp
+
+
+def create_simple_frame():
+    hdr = {'INSTRUME': 'MEGARA', 'INSCONF': 'ca3558e3-e50d-4bbc-86bd-da50a0998a48'}
+    hdu = fits.PrimaryHDU(data=[[1]])
+    for k, v in hdr.items():
+        hdu.header[k] = v
+    hdulist = fits.HDUList([hdu])
+    frame = numina.core.DataFrame(frame=hdulist)
+    return frame
 
 
 @pytest.mark.parametrize("conf, uuix", [
@@ -15,6 +27,7 @@ def test_loader1(conf, uuix):
     from numina.instrument.assembly import assembly_instrument
 
     obs = numina.core.ObservationResult(instrument='MEGARA')
+    obs.frames.append(create_simple_frame())
     drpm = load_drp()
     obs.configuration = conf
 
@@ -22,42 +35,3 @@ def test_loader1(conf, uuix):
     ins = assembly_instrument(drpm.configurations, key, date_obs, by_key=keyname)
     assert isinstance(ins, numina.instrument.generic.InstrumentGeneric)
     assert str(ins.origin.uuid) == uuix
-
-
-@pytest.mark.parametrize("uuix, uuixc", [
-    ["ca3558e3-e50d-4bbc-86bd-da50a0998a48",
-     ['2e02e135-2325-47c9-9975-466b445b0b8b', '78f6d437-85d6-4d7a-bdb7-1e043368b442']
-     ],
-    ["9a86b2b2-3f7d-48ec-8f4f-3780ec967c90",
-     ['2e02e135-2325-47c9-9975-466b445b0b8b', '97d48545-3258-473e-9311-00e390999d52']],
-    ["66f2283e-3049-4d4b-8ef1-14d62fcb611d",
-     ['2e02e135-2325-47c9-9975-466b445b0b8b', '715c7ced-f989-42a6-bb74-a5b5cda0495c']],
-    ["4fd05b24-2ed9-457b-b563-a3c618bb1d4c",
-     ['2e02e135-2325-47c9-9975-466b445b0b8b', '78f6d437-85d6-4d7a-bdb7-1e043368b442']]
-])
-def test_subconf(uuix, uuixc):
-    import numina.core
-    from numina.instrument.assembly import assembly_instrument
-
-    obs = numina.core.ObservationResult(instrument='MEGARA')
-    drpm = load_drp()
-    obs.configuration = uuix
-
-    key, date_obs, keyname = drpm.select_profile(obs)
-    ins = assembly_instrument(drpm.configurations, key, date_obs, by_key=keyname)
-    for c,uu in zip(ins.children.values(), uuixc):
-        assert str(c.origin.uuid) == uu
-
-
-
-def represent_comp(comp):
-    print('name', comp.name)
-    print('parent', comp.parent)
-    print('properties', comp.properties)
-    for c,k in comp.children.items():
-        represent_comp(k)
-    print('oname:', comp.origin.name)
-    print('ouuid=', comp.origin.uuid)
-
-    print('odate_start=', comp.origin.date_start)
-    print('odate_end=', comp.origin.date_end)
