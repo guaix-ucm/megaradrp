265fb062d9d55e4a457f34775a24e4dfcd54bb27
Sergio Pascual <sergiopr@fis.ucm.es>
2020-06-29 13:38:53 +0200

    Update to v0.11.dev0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='megaradrp',
-    version='0.10.dev0',
+    version='0.11.dev0',
     author='Sergio Pascual',
     author_email='sergiopr@fis.ucm.es',
     url='https://github.com/guaix-ucm/megaradrp',
