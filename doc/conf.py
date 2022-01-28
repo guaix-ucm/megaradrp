3d858d7e35009985f1b9500488e4634347d33c13
Sergio Pascual <sergiopr@fis.ucm.es>
2022-01-28 09:40:59 +0100

    Release v0.12.0

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ exclude_patterns = ['_build']
 project = u'MEGARA Data Reduction Pipeline'
 copyright = u'2013-2021, Universidad Complutense de Madrid'
 version = '0.12'
-release = '0.12.dev0'
+release = '0.12.0'
 show_authors = True
 
 numpydoc_show_class_members = False
