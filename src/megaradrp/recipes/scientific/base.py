a5ce96f89c91b26e0b783b4b8d0648bec2738be8
Nicolás Cardiel <cardiel@ucm.es>
2025-07-09 19:05:59 +0200

    Make use of mediancr method defined in numina

--- src/megaradrp/recipes/scientific/base.py
+++ src/megaradrp/recipes/scientific/base.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2011-2023 Universidad Complutense de Madrid
+# Copyright 2011-2025 Universidad Complutense de Madrid
 #
 # This file is part of Megara DRP
 #
@@ -36,7 +36,7 @@ class ImageRecipe(MegaraBaseRecipe):
     method = Parameter(
         'median',
         description='Combination method',
-        choices=['mean', 'median', 'sigmaclip']
+        choices=['mean', 'median', 'mediancr', 'sigmaclip']
     )
     method_kwargs = Parameter(
         dict(),
